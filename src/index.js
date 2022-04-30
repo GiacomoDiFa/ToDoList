8314edd04a6183b429a4d7dc85a09afcc5753f46
GiacomoDiFa <giacomodfabrizio@gmail.com>
2022-04-30 11:25:20 +0200

    try

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { instance } from './addAndRemove';
-import { dragTask } from './reorder';
-import './style.css';
-
-window.onload = () => {
-  instance.editTask();
-  instance.updateStatuses();
-  dragTask.drag();
-};
